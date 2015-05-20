8802608823b7f15df372691eff228623b6058d70
CalvinZheng <calvinz.mcgill@gmail.com>
2015-05-20 15:16:25 -0400

    Initial Commit

---
+++ socialQ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socialQ
+//
+//  Created by Calvin Zheng on 2015-05-20.
+//  Copyright (c) 2015 JakeS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
